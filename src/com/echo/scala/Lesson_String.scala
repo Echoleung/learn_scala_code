45df6e27fcc83d0e997630f7686eae2b92fe89ad
Echoleung <lwt1689@gmail.com>
2020-11-22 21:52:04 +0800

    ScalaString

---
+++ src/com/echo/scala/Lesson_String.scala
@@ -0,0 +1,14 @@
+package com.echo.scala
+
+object Lesson_String {
+  def main(args: Array[String]): Unit = {
+    val s = "echo"
+    val s1 = "ECHO"
+
+//    println(s.indexOf(99))//返回ascll码98所对应的字符在字符串中第一次出现的位置
+//    println(s.indexOf(98))//不存在就返回-1
+
+//    println(s.equals(s1))//比对
+//    println(s.equalsIgnoreCase(s1))//字符串比对忽略大小写
+  }
+}
