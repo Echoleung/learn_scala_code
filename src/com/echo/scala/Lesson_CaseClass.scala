0292fa4ac29957742d659f1f6dc90e46d399bdf8
Echoleung <lwt1689@gmail.com>
2020-11-23 00:45:58 +0800

    Scala样例类

---
+++ src/com/echo/scala/Lesson_CaseClass.scala
@@ -0,0 +1,14 @@
+package com.echo.scala
+
+case class Person1(name:String,age:Int){
+
+}
+
+object Lesson_CaseClass {
+  def main(args: Array[String]): Unit = {
+    val p1 = new Person1("zhangsan",18)
+    val p2 = new Person1("zhangsan",18)
+    println(p1.equals(p2))
+    println(p1)
+  }
+}
